4d0f88db0aac6c32701bbceee127693c42393bff
andikarafon <andika.rafon.senohaji@gmail.com>
2023-05-02 11:37:22 +0700

    API Register done

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,8 +26,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',        
-        'username',        
-        'phone_number',        
+        'username',         
         'roles',        
         'password',
     ];
