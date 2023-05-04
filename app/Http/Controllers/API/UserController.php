89129517b3eaba294f2e627c584ff695133b29e0
andikarafon <andika.rafon.senohaji@gmail.com>
2023-05-04 14:08:53 +0700

    API user dan update profile DONE

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,13 +7,14 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-// use illuminate\Support\Facades\Auth;
+use illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 // use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
+
     public function register(Request $request)
     {
         try {
@@ -49,4 +50,62 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            
+            $request->validate([
+                'email'     => 'email|required',
+                'password'  => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if(!Auth::attempt($credentials)) 
+            {
+                return ResponseFormatter::error([
+                    'message'       => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if(! Hash::check($request->password, $user->password, []))
+            {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            // jika berhasil
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token'      => $tokenResult,
+                'token_type'        => 'Bearer',
+                'user'              => $user
+            ], 'Authenticated');
+ 
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message'       => 'Something went wrong',
+                'error'         => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'Data Profile User berhasil diambil');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
 }
